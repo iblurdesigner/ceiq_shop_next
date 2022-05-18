1c8ad2365d5aebd4413f12032a1b19e97494e664
iblurdesigner <nevomen@gmail.com>
2022-05-18 15:45:20 -0500

    creacion de registro de usuario

--- pages/register.js
+++ pages/register.js
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import dynamic from 'next/dynamic';
 
-function Login() {
+function Register() {
   const router = useRouter();
   const { redirect } = router.query; // esto es para que redirija a login?redirect=/shipping
   const { state, dispatch } = useContext(Store);
@@ -20,12 +20,21 @@ function Login() {
     }
   }, []);
 
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const submitHandler = async (e) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert('Las contraseñas no son las mismas');
+      return;
+    }
+
     try {
-      const { data } = await axios.post('/api/users/login', {
+      const { data } = await axios.post('/api/users/register', {
+        name,
         email,
         password,
       });
@@ -39,15 +48,25 @@ function Login() {
   };
 
   return (
-    <Layout title="Ingresar">
+    <Layout title="Registro">
       <div className="flex justify-center">
         <div className="card w-4/12 ">
           <form
             onSubmit={submitHandler}
             className="rounded px-8 pt-6 pb-8 mb-4"
           >
-            <h1 className="text-4xl py-4">Ingresar</h1>
+            <h1 className="text-4xl py-4">Registro</h1>
             <List>
+              <ListItem>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="name"
+                  label="Nombre"
+                  inputProps={{ type: 'text' }}
+                  onChange={(e) => setName(e.target.value)}
+                ></TextField>
+              </ListItem>
               <ListItem>
                 <TextField
                   variant="outlined"
@@ -69,17 +88,27 @@ function Login() {
                 ></TextField>
               </ListItem>
               <ListItem>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="confirmPassword"
+                  label="Confirmar Contraseña"
+                  inputProps={{ type: 'password' }}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></TextField>
+              </ListItem>
+              <ListItem>
                 <button
                   className="bg-green py-2 px-8 shadow-md rounded-full hover:bg-cyan"
                   type="submit"
                 >
-                  Ingresar
+                  Registrar
                 </button>
               </ListItem>
               <ListItem>
-                <p className="mx-2">¿Aún no tienes una cuenta?</p>
-                <Link href={`/register?redirect=${redirect || '/'}`} passHref>
-                  <a className="text-green">Registrarse</a>
+                <p className="mx-2">¿Ya tienes una cuenta?</p>
+                <Link href={`/login?redirect=${redirect || '/'}`} passHref>
+                  <a className="text-green">Ingresar</a>
                 </Link>
               </ListItem>
             </List>
@@ -92,4 +121,4 @@ function Login() {
 
 //esto es para evitar el error de Hydration
 
-export default dynamic(() => Promise.resolve(Login), { ssr: false });
+export default dynamic(() => Promise.resolve(Register), { ssr: false });
