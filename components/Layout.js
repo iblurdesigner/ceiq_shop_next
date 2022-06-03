49e7eef20fb6ba09e525715ee9a2ee6f0918ec53
iblurdesigner <nevomen@gmail.com>
2022-06-03 18:40:17 -0500

    Sidebar con menu hamburguesa para busqueda por categorias

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 // import { useTheme } from 'next-themes';
 // import Button from './Button';
 import Head from "next/head";
@@ -7,9 +7,23 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
 import { Store } from "../utils/Store";
-import { Menu, MenuItem } from "@mui/material";
+import { getError } from "../utils/error";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 
 import dynamic from "next/dynamic";
+import Button from "./Button";
+import { useSnackbar } from "notistack";
+import axios from "axios";
 const ButtonDarkM = dynamic(() => import("./ButtonDarkM"), { ssr: false });
 
 export default function Layout({ title, description, children }) {
@@ -18,6 +32,29 @@ export default function Layout({ title, description, children }) {
   const { cart, userInfo } = state;
 
   const [anchorEl, setAnchorEl] = useState(null);
+  const [sidbarVisible, setSidebarVisible] = useState(false);
+
+  const sidebarOpenHandler = () => {
+    setSidebarVisible(true);
+  };
+  const sidebarCloseHandler = () => {
+    setSidebarVisible(false);
+  };
+
+  const [categories, setCategories] = useState([]);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`/api/products/categories`);
+      setCategories(data);
+    } catch (err) {
+      enqueueSnackbar(getError(err), { variant: "error" });
+    }
+  };
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   const loginClickHandler = (e) => {
     setAnchorEl(e.currentTarget);
@@ -48,16 +85,94 @@ export default function Layout({ title, description, children }) {
       <div className="dark:flex min-h-screen flex-col justify-between ">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md navbar">
-            <Link href="/" passHref>
-              <a className="text-lg font-bold">
-                <Image
-                  src="/logo_ceiqShop.svg"
-                  width={280}
-                  height={64}
-                  alt="logo"
-                />
-              </a>
-            </Link>
+            <Box display="flex" alignItems="center">
+              <IconButton
+                edge="start"
+                aria-label="open drawer"
+                onClick={sidebarOpenHandler}
+              >
+                <Button className="text-white px-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </Button>
+                <Link href="/" passHref>
+                  <a className="text-lg font-bold">
+                    <Image
+                      src="/logo_ceiqShop.svg"
+                      width={280}
+                      height={64}
+                      alt="logo"
+                    />
+                  </a>
+                </Link>
+              </IconButton>
+            </Box>
+
+            <Drawer
+              anchor="left"
+              open={sidbarVisible}
+              onClose={sidebarCloseHandler}
+            >
+              <List>
+                <ListItem>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <p className="text-xl text-gray-400 font-light">
+                      Búsqueda por categoría
+                    </p>
+                    <IconButton
+                      aria-label="close"
+                      onClick={sidebarCloseHandler}
+                    >
+                      <Button className="text-green hover:bg-green hover:text-blue">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </Button>
+                    </IconButton>
+                  </Box>
+                </ListItem>
+                <Divider light />
+                {categories.map((category) => (
+                  <Link
+                    key={category}
+                    href={`/search?category=${category}`}
+                    passHref
+                  >
+                    <ListItem
+                      button
+                      component="a"
+                      onClick={sidebarCloseHandler}
+                    >
+                      <ListItemText primary={category}></ListItemText>
+                    </ListItem>
+                  </Link>
+                ))}
+              </List>
+            </Drawer>
+
             <div className="w-64  flex justify-between">
               <ButtonDarkM />
 
