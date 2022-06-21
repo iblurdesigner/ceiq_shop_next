1cf6a26168ed92fdec7cf673fcd76f962d8af939
iblurdesigner <nevomen@gmail.com>
2022-06-21 14:56:59 -0500

    configuracion de lighhouse con cypress

---
+++ cypress/support/index.js
@@ -0,0 +1,13 @@
+import "@cypress-audit/lighthouse/commands";
+// cypress/plugins/index.js
+const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
+
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    lighthouse: lighthouse(), // calling the function is important
+  });
+};
