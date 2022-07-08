ed068b4aa3d50f4b5e03860c4807a37db53e52c3
iblurdesigner <nevomen@gmail.com>
2022-07-08 11:50:15 -0500

    performance fase 5

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import { Store } from "../utils/Store";
@@ -59,101 +59,100 @@ export default function Layout({ title, description, children }) {
         {description && <meta name="description" content={description} />}
         <link rel="icon" href="/favicon01.svg" />
       </Head>
-      <Suspense fallback={`Loading...`}>
-        <div className="dark:flex min-h-screen flex-col justify-between">
-          <header>
-            <nav className="flex navbar items-center px-4 justify-center flex-wrap sm:flex-nowrap md:justify-between shadow-md h-40 md:h-28">
-              <SideMenu
-                props={
-                  (logoutClickHandler, logoutClickHandler, loginClickHandler)
-                }
-              />
-              <div className="lg:w-4/6 flex sm:flex-wrap justify-between items-center md:px-6">
-                <Search />
-                <div className="invisible md:visible order-3 md:order-2">
-                  <ButtonDarkM
-                    aria-describedby="boton-modo-oscuro"
-                    alt="boton modo oscuro"
-                  />
-                </div>
+      <div className="dark:flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex navbar items-center px-4 justify-center flex-wrap sm:flex-nowrap md:justify-between shadow-md h-40 md:h-28">
+            <SideMenu
+              props={
+                (logoutClickHandler, logoutClickHandler, loginClickHandler)
+              }
+            />
+            <div className="lg:w-4/6 flex sm:flex-wrap justify-between items-center md:px-6">
+              <Search />
+              <div className="invisible md:visible order-3 md:order-2">
+                <ButtonDarkM
+                  aria-describedby="boton-modo-oscuro"
+                  alt="boton modo oscuro"
+                />
+              </div>
 
-                <div className="order-2 md:order-3">
-                  <Cart cart={cart} />
-                </div>
+              <div className="order-2 md:order-3">
+                <Cart cart={cart} />
+              </div>
 
-                <div className="order-last visible">
-                  {userInfo ? (
-                    <>
-                      <button
-                        data-test="user-button"
-                        className="hover:text-green mx-6"
-                        type="button"
-                        alt="boton usuario"
-                        aria-controls="simple-menu"
-                        aria-haspopup="true"
-                        onClick={loginClickHandler}
+              <div className="order-last visible">
+                {userInfo ? (
+                  <>
+                    <button
+                      data-test="user-button"
+                      className="hover:text-green mx-6"
+                      type="button"
+                      alt="boton usuario"
+                      aria-controls="simple-menu"
+                      aria-haspopup="true"
+                      onClick={loginClickHandler}
+                    >
+                      {userInfo.name}
+                    </button>
+
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={anchorEl}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={loginMenuCloseHandler}
+                    >
+                      <MenuItem
+                        onClick={(e) => loginMenuCloseHandler(e, "/profile")}
                       >
-                        {userInfo.name}
-                      </button>
-
-                      <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={loginMenuCloseHandler}
+                        Perfil
+                      </MenuItem>
+                      <MenuItem
+                        onClick={(e) =>
+                          loginMenuCloseHandler(e, "/order-history")
+                        }
                       >
-                        <MenuItem
-                          onClick={(e) => loginMenuCloseHandler(e, "/profile")}
-                        >
-                          Perfil
-                        </MenuItem>
+                        Historial de órdenes
+                      </MenuItem>
+                      {userInfo.isAdmin && (
                         <MenuItem
                           onClick={(e) =>
-                            loginMenuCloseHandler(e, "/order-history")
+                            loginMenuCloseHandler(e, "/admin/dashboard")
                           }
                         >
-                          Historial de órdenes
+                          Administración Dashboard
                         </MenuItem>
-                        {userInfo.isAdmin && (
-                          <MenuItem
-                            onClick={(e) =>
-                              loginMenuCloseHandler(e, "/admin/dashboard")
-                            }
-                          >
-                            Administración Dashboard
-                          </MenuItem>
-                        )}
-                        <MenuItem
-                          data-test="logout-button"
-                          onClick={logoutClickHandler}
-                        >
-                          Cerrar Sesion
-                        </MenuItem>
-                      </Menu>
-                    </>
-                  ) : (
-                    <Link href="/login" passHref>
-                      <a className="p-2">
-                        <Userbtn
-                          alt="boton usuario"
-                          className="text-white h-6 w-6"
-                        />
-                      </a>
-                    </Link>
-                  )}
-                </div>
+                      )}
+                      <MenuItem
+                        data-test="logout-button"
+                        onClick={logoutClickHandler}
+                      >
+                        Cerrar Sesion
+                      </MenuItem>
+                    </Menu>
+                  </>
+                ) : (
+                  <Link href="/login" passHref>
+                    <a className="p-2">
+                      <Userbtn
+                        alt="boton usuario"
+                        className="text-white h-6 w-6"
+                      />
+                    </a>
+                  </Link>
+                )}
               </div>
-            </nav>
-          </header>
-          <main className="container m-auto mt-4 px-4 min-h-screen">
-            {children}
-          </main>
-          <footer className="flex h-10 justify-center items-center shadow-inner">
-            <p>Copyright © 2022 CEIQ</p>
-          </footer>
-        </div>
-      </Suspense>
+            </div>
+          </nav>
+        </header>
+
+        <main className="container m-auto mt-4 px-4 min-h-screen">
+          {children}
+        </main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 CEIQ</p>
+        </footer>
+      </div>
     </>
   );
 }
