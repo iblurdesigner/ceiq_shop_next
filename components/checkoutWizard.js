5e85ed680ee4819789db500e4a99088b99d7a383
iblurdesigner <nevomen@gmail.com>
2022-07-11 17:52:19 -0500

    nuevo arreglo del nombre checkoutwizard

--- components/checkoutWizard.js
+++ components/checkoutWizard.js
@@ -1,7 +1,6 @@
 import { Step, StepLabel, Stepper } from "@mui/material";
 import React from "react";
 
-// habia que renombrar el archivo con letra mayuscula al inicio
 export default function CheckoutWizard({ activeStep = 0 }) {
   return (
     <Stepper activeStep={activeStep} alternativeLabel>
